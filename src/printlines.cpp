04b3e97d307100069684b3ecebf9bd721ede060a
Victor Chung <kindlychung@gmail.com>
2014-08-11 01:12:09 +0200

    replace symlinks with hard links

--- src/printlines.cpp
+++ src/printlines.cpp
@@ -1,1 +1,17 @@
-../../cpp/cppPlayground/src/printlines.cpp
+/*
+ * printlines.cpp
+ *
+ *  Created on: Jul 26, 2014
+ *      Author: kaiyin
+ */
+#include "printlines.h"
+
+
+void printlines(std::string fn) {
+	std::ifstream infile(fn);
+	while(infile) {
+		std::string l;
+		std::getline(infile, l);
+		std::cout << l << "\n";
+	}
+}
--- src/printlines.h
+++ src/printlines.h
@@ -1,1 +1,19 @@
-../../cpp/cppPlayground/src/printlines.h
+/*
+ * printlines.h
+ *
+ *  Created on: Jul 26, 2014
+ *      Author: kaiyin
+ */
+
+#ifndef PRINTLINES_H_
+#define PRINTLINES_H_
+
+#include <fstream>
+#include <iostream>
+
+void printlines(std::string fn);
+
+
+
+
+#endif /* PRINTLINES_H_ */
